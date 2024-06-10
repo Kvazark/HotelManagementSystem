a7c4c2fdc7bae6a8908b23ffe2c280817cdb279d
first <Katya_Klebleeva@mail.ru>
2024-06-10 22:53:37 +0300

    added consumer project

---
+++ Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+class Consumer {
+
+    static void Main(string[] args)
+    {
+        const string topic = "RoomSelected";
+        var consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "consumer-group-1",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+     
+        using (var consumer = new ConsumerBuilder<Ignore, string>(
+                   consumerConfig).Build())
+        {
+            try {
+                consumer.Subscribe(topic);
+                
+                while (true) {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key} value = {cr.Message.Value}");
+                }
+            }
+            catch (ConsumeException e) {
+                Console.WriteLine($"Error occured: {e.Error.Reason}");
+            }
+            catch (OperationCanceledException) {
+                
+            }
+            finally{
+                consumer.Close();
+            }
+        }
+    }
+}
