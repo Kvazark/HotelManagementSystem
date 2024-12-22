058869490a87de5b0dd7ce87248c6702bb4f3f9f
first <Katya_Klebleeva@mail.ru>
2024-12-22 13:33:54 +0300

    added aggregator

--- Gateway/obj/Debug/net8.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net8.0/Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07c4406cf54685fa77b589dee01420c026aa60a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d2a143c61e423387fdbfe2f20487cdf7df2774")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
